2df2d2ebad2dc58828495f66c74719ea462b1b14
hermanj13 <hermanj13@me.com>
2017-03-03 17:42:40 -0500

    media

--- Platos/urls.py
+++ Platos/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
 	static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
